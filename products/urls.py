cb21feda7ad963f4ff5cac03bedfe59e2c51c89e
SashaKotton <kotton2011@gmail.com>
2024-02-16 20:26:32 +0500

    added media_url and media_root tot static, added new url to product detail information

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from products.views import product_list
+from products.views import product_list, product_detail
 
 urlpatterns = [
-    path('', product_list, name='product_list')
+    path('', product_list, name='product_list'),
+    path('detail/<int:pk>', product_detail, name='product_detail')
 ]
