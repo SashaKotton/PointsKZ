02b124c4a7fcfacbed0012df8096e66f19489229
SashaKotton <kotton2011@gmail.com>
2024-02-21 11:34:22 +0500

    added static. and collected it

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from products.views import product_list, product_detail
+from products.views import product_list, product_detail, add_new_product, edit_product
 
 urlpatterns = [
     path('', product_list, name='product_list'),
-    path('detail/<int:pk>', product_detail, name='product_detail')
+    path('detail/<int:pk>', product_detail, name='product_detail'),
+    path('new/', add_new_product, name='add_new_product'),
+    path('edit/<int:pk>', edit_product, name = 'edit_product')
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from products.models import Product
+from products.forms import ProductForm
 
 # Create your views here.
 def product_list(request):
@@ -9,3 +10,28 @@ def product_list(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product.objects.all(), pk=pk)
     return render(request, 'products/product_detail.html', {'product': product})
+
+def add_new_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            product = form.instance
+            return redirect('product_detail', pk=product.pk)
+        pass
+    else:
+        form = ProductForm()
+    return render(request, 'products/add_new_product.html', {'form': form })
+
+def edit_product(request, pk):
+    product = get_object_or_404(Product.objects.all(), pk = pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            product = form.instance
+            return redirect('product_detail', pk=product.pk)
+        pass
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'products/add_new_product.html', {'form': form, 'product': product })
